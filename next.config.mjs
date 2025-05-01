bca5a9f38b625323993bc683c3d5f31ac10037c7
50Fifty <jenseanfoo@gmail.com>
2025-05-01 15:18:03 +0800

    Revert "fix: update next.config.mjs to set basePath and assetPrefix based on production environment"
    This reverts commit 53afce200dfa79dae59ab267e61b156e8862d157.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {
-  basePath: isProd ? '/ricart-agrawala' : '',
-  assetPrefix: isProd ? '/ricart-agrawala/' : '',
-  trailingSlash: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
