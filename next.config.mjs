4f5518b508156f64d48ac6b4f0497b076d3f23f7
50Fifty <jenseanfoo@gmail.com>
2025-05-01 15:34:42 +0800

    fix: export

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  basePath: isProd ? '/ricart-agrawala' : '',
-  assetPrefix: isProd ? '/ricart-agrawala/' : '',
-  trailingSlash: true,
+  output: 'export',
 };
 
 export default nextConfig;
